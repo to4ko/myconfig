0342a2d0e6b46bb4fb3ab7d0fd9efc0716e44919
Dmitry Tsymbal <dtsymbal@slb.com>
2019-05-26 22:42:22 +0300

    daily 26.05.2019

--- custom_components/custom_updater/__init__.py
+++ custom_components/custom_updater/__init__.py
@@ -15,7 +15,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '5.0.0'
+VERSION = '5.1.0'
 
 _LOGGER = logging.getLogger(__name__)
 
