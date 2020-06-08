d0450642c57117979563629467588696c2aa4d41
Dmitry Tsymbal <tsymbal@rambler.ru>
2020-06-08 23:32:39 +0300

    08.06.2020

--- custom_components/start_time/sensor.py
+++ custom_components/start_time/sensor.py
@@ -1,6 +1,5 @@
 import logging
 from collections import OrderedDict
-from datetime import datetime
 from logging import LogRecord
 
 from homeassistant.helpers.entity import Entity
@@ -13,18 +12,23 @@ DOMAIN = 'start_time'
 
 
 class LogsHandler(logging.Handler):
-    def __init__(self):
-        super().__init__()
-        self.add_entities = None
-        self.attrs = {}
+    state = None
+    attrs = {}
+    add_entities = None
 
     def handle(self, record: LogRecord) -> None:
         if record.msg.startswith("Setup of domain"):
             self.attrs[record.args[0]] = round(record.args[1], 1)
 
-        elif (record.msg.startswith("Home Assistant initialized") and
-              self.add_entities):
-            self.add_entities([StartTime(record.args[0], self.attrs)])
+        elif record.msg.startswith("Home Assistant initialized"):
+            self.state = round(record.args[0], 1)
+            at = sorted(self.attrs.items(), key=lambda t: t[1], reverse=True)
+            self.attrs = OrderedDict(at)
+            self.update()
+
+    def update(self):
+        if self.state and self.add_entities:
+            self.add_entities([StartTime(self.state, self.attrs)])
 
 
 handler = LogsHandler()
@@ -36,14 +40,14 @@ logging.getLogger('homeassistant.setup').addHandler(handler)
 async def async_setup_platform(hass: HomeAssistantType, config, add_entities,
                                discovery_info=None):
     handler.add_entities = add_entities
+    handler.update()
     return True
 
 
 class StartTime(Entity):
-    def __init__(self, duration: float, attrs: dict):
-        attrs = sorted(attrs.items(), key=lambda t: t[1], reverse=True)
-        self._attrs = OrderedDict([('datetime', datetime.now())] + attrs)
-        self._state = round(duration, 1)
+    def __init__(self, state: float, attrs: dict):
+        self._state = state
+        self._attrs = attrs
 
     @property
     def should_poll(self) -> bool:
